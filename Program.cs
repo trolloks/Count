ebf8ef97d654ab848e79f641d3158b9717b7ddef
DESKTOP-DDHD624\Trolloks <rikuswlouw@gmail.com>
2019-02-12 17:09:48 +0200

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialise menu
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("THE COUNT");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("1. Start New Game");
+
+        }
+    }
+}
